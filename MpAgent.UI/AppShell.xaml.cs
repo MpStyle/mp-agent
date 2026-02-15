2807a52eab9e7752e991a486fc28fa18af36c2d8
Michele Pagnin <MpStyle@users.noreply.github.com>
2026-02-15 09:02:35 +0100

    Add initial UI project structure and components for MpAgent.UI

---
+++ MpAgent.UI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MpAgent.UI;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MpAgent.UI/Features/GitLabMergeRequestReviewer/GitLabMergeRequestReviewer.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MpAgent.UI.Features.GitLabMergeRequestReviewer;
+
+public partial class GitLabMergeRequestReviewer : ContentView
+{
+    public GitLabMergeRequestReviewer()
+    {
+        InitializeComponent();
+    }
+}
